a7fad0b497651f555cc56a199286e220bfe47b75
Talha Anwar <mtalhaanwar359@gmail.com>
2024-04-25 16:29:49 +0500

    Adding the nodemon

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const app = express();
 
-console.dir(app);
+// console.dir(app);
 
-let port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-app.use( (req, res) => {
-    console.log('Received request from user');
+const port = 3000;
+// app.use( port, (req, res) => {
+//     console.log('Received request from user');
+//     res.send({ 
+//         message: 'Hello, World!',
+//         status: 200
+//     });
+//   });
+app.get( "/", (req, res) => {
     res.send({ 
         message: 'Hello, World!',
         status: 200
     });
   });
+app.get( "/nothing", (req, res) => {
+    res.send({ 
+        message: 'nothing here!',
+        status: 200
+    });
+  });
+app.get( "*", (req, res) => {
+    res.send({ 
+        message: 'You are on a worng path!',
+        status: 200
+    });
+  });
+  
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
